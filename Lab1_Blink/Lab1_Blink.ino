981d872b85872714ec9e2d1f7a866817358088c6
Andrew Martin <andrewm1177@gmail.com>
2020-02-13 19:36:29 -0600

    wrote timedblink function

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -1,8 +1,33 @@
+#define LED_PIN 2
 void setup() {
   // put your setup code here, to run once:
+    // initialize digital pin LED_PIN as an output.
+  pinMode(LED_PIN, OUTPUT);
 }
-
+void dimmer(int freq, int duty) {
+    int period, onTime, offTime;
+    period = 1000/freq;
+    onTime = period * duty / 100;
+    offTime = period - onTime;
+    digitalWrite(LED_PIN, HIGH);
+    delay(onTime);
+    digitalWrite(LED_PIN, LOW);
+    delay(offTime);
+}
+void timedblink(int t) {
+   digitalWrite(LED_PIN, HIGH);
+   delay(t);
+   digitalWrite(LED_PIN, LOW);
+   delay(t);
+   
+}
 void loop() {
   // put your main code here, to run repeatedly:
-
+  //digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  //delay(500);                       // wait for a second
+  //digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
+  //delay(500);                       // wait for a second
+  timedblink(250);
+  timedblink(500);
+  timedblink(1000);
 }
